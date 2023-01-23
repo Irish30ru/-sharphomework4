33da329a9a7a0814fd46b7dd03e1f7297f67e291
Andrey <akokinn2003@yandex.ru>
2023-01-23 12:17:19 +0400

    Task2 complete

--- homework4.cs
+++ homework4.cs
@@ -1,1 +1,36 @@
-﻿
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+int count = 0;
+int countNum(int numA)
+{   
+    // int count = 0;
+    while (numA != 0)
+    {
+        numA = numA / 10;
+        count += 1;
+    }
+    return count;
+}
+
+int sumNum (int numA, int lenght) 
+{
+    int length = count;
+    int result = 0;
+    for (int i = 0; i <=lenght; i++)
+    {
+        result +=  numA % 10;
+        numA = numA / 10;
+    }
+    return result;
+}
+Console.Write("Введите число:");
+int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(countNum(82));
+Console.WriteLine($"Сумма цифр = {sumNum(x,countNum(x))}");
+
+// Console.WriteLine();
